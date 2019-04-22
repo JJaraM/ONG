d0136a6ae84d08d05c1a869ccc84e0d25afa05a2
Jara <jnja@gft.com>
2019-04-22 15:16:55 -0600

    JJ: More changes :(

--- express.js
+++ express.js
@@ -6,7 +6,7 @@ const sourceDir = 'dist';
 app.use(express.static(sourceDir));
 
 app.get('/', function(req, res){
-    const path = 'index.html';
+    const path = '/index.html';
     console.log('sending to ' + path);
     res.sendFile(path);
 });
