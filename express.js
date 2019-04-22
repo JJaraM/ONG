45fe4b3eadef9f4891c91a0d6b72a8d7753d50af
Jara <jnja@gft.com>
2019-04-22 15:59:03 -0600

    JJ: More changes :(

--- express.js
+++ express.js
@@ -4,10 +4,11 @@ var path = require('path');
 var port = process.env.PORT || 8080;
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+console.log('Dist Folder: ' + path.join(__dirname, '../dist/index.html'));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('*', (_req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(port, (err) => {
