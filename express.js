401df0c986320db4cfabb10fa7140a4bebb58da0
Jara <jnja@gft.com>
2019-04-21 20:40:59 -0600

    JJ: Another try

--- express.js
+++ express.js
@@ -1,17 +1,11 @@
 const express = require('express');
-const path = require('path');
-const port = process.env.PORT || 8080;
 const app = express();
+const portNumber = 8080;
+const sourceDir = 'dist';
 
-// the __dirname is the current directory from where the script is running
-app.use(express.static(__dirname));
+app.use(express.static(sourceDir));
 
-// send the user to index html page inspite of the url
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
-app.listen(port, () => {
-  console.log(`Express web server started: http://localhost:${port}`);
+app.listen(portNumber, () => {
+  console.log(`Express web server started: http://localhost:${portNumber}`);
   console.log(`Serving content from /${sourceDir}/`);
 });
